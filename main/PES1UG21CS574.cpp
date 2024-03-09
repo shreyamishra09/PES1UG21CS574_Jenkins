1087dffbe5a51533a5d92cfbf8699584c277a375
shreyamishra09 <162302584+shreyamishra09@users.noreply.github.com>
2024-03-09 21:30:02 +0530

    Update PES1UG21CS574.cpp

--- main/PES1UG21CS574.cpp
+++ main/PES1UG21CS574.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(void) {
-  cout<< "Hello Adrija\n";
+  cout<< "Hello Shreya Mishra\n";
   cout << "My SRN is PES1UG21CS574\n";
   cout << "This is CC assignment and task-5\n";
   return 0;
