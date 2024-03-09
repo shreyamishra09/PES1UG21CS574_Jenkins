f183d624c9355500c99d0bf8e85b4e02c427dca4
shreyamishra09 <162302584+shreyamishra09@users.noreply.github.com>
2024-03-09 21:23:50 +0530

    Create PES1UG21CS574.cpp

---
+++ main/PES1UG21CS574.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout<< "Hello Adrija\n";
+  cout << "My SRN is PES1UG21CS574\n";
+  cout << "This is CC assignment and task-5\n";
+  return 0;
+}
